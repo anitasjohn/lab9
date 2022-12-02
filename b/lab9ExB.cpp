6db21ee419ea5b8e9126ccd73809af796cdfe9d5
dietban19 <dieter.banaag@ucalgary.ca>
2022-12-01 20:45:24 -0700

    final

--- b/lab9ExB.cpp
+++ b/lab9ExB.cpp
@@ -4,14 +4,19 @@
 #include <sstream>
 #include <stdlib.h>
 
-const int SIZE = 6;
-using namespace std;
+const int size = 6;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+using std::cerr;
 struct City {
     double x, y;
     char name[30];
 };
 
-void write_binary_file(City cities[], int SIZE, char* filename);
+void write_binary_file(City cities[], int size, char* filename);
 /* PROMISES: attaches an ofstream object to a binary file named "filename" and
  * writes the content of the array cities into the file.
  */
@@ -24,27 +29,27 @@ void print_from_binary(char* filename);
 int main() {
     char bin_filename[] = "cities.bin";
     
-    City cities[SIZE] = {{100, 50, "Calgary"},
+    City cities[size] = {{100, 50, "Calgary"},
         {100, 150, "Edmonton"},
         {50, 50, "Vancouver"},
         {200, 50, "Regina"},
         {500, 50, "Toronto"},
         {200, 50, "Montreal"}};
     
-    write_binary_file(cities, SIZE, bin_filename);
+    write_binary_file(cities, size, bin_filename);
     cout << "\nThe content of the binary file is:" << endl;
     print_from_binary(bin_filename);
     return 0;
 }
 
-void write_binary_file(City cities[], int SIZE, char* filename){
+void write_binary_file(City cities[], int size, char* filename){
     ofstream stream(filename, ios::out | ios::binary);
     if(stream.fail()){
         cerr << "failed to open file: " << filename << endl;
         exit(1);
     }
     
-    for(int i =0; i < SIZE; i++)
+    for(int i =0; i < size; i++)
         stream.write((char*)&cities[i], sizeof(City));
     stream.close();
 }
@@ -56,7 +61,7 @@ void print_from_binary(char* filename) {
         cerr << "Failed to Open File: " << filename << endl;
         exit(1);
     }
-    for(int i = 0; i < SIZE; i++){
+    for(int i = 0; i < size; i++){
         is.read(reinterpret_cast<char*>(&cityName[i]), sizeof(cityName));
         cout << "Name: " << cityName[i].name;
         cout << ", x coordinate: " << cityName[i].x;
